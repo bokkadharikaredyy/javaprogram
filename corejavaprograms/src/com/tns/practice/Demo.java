3e9dc6b21632235f813adb87c98650de9b93e318
bokka <bokka@LAPTOP-R3S71M4M>
2024-08-25 21:31:48 +0530

    commit message

---
+++ corejavaprograms/src/com/tns/practice/Demo.java
@@ -0,0 +1,8 @@
+package com.tns.practice;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("hi hello");
+	}
+
+}
---
+++ corejavaprograms/src/defaultpackage/Helloworldtest.java
@@ -0,0 +1,12 @@
+package defaultpackage;
+
+import org.junit.jupiter.api.Test;
+
+public class Helloworldtest {
+	@Test
+	void print{
+		System.out.println("hello");
+		
+	}
+
+}
